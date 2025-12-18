12b750a4a71c34fd60919eb6a62a25dda2a1be6b
Hua Minh Bao <hmbaodev>
2025-12-18 12:56:24 +0700

    feat: add hero animation

--- section/Hero.tsx
+++ section/Hero.tsx
@@ -1,18 +1,37 @@
-import Image from "next/image";
+"use client";
+
+import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useTransform,
+} from "motion/react";
 
 import cylinderImage from "@/assets/cylinder.png";
 import cogImage from "@/assets/cog.png";
 import noodleImage from "@/assets/noodle.png";
 
 const Hero = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+  // useMotionValueEvent(scrollYProgress, "change", (latestValue) => {
+  //   console.log(latestValue);
+  // });
+
   return (
-    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183ec2,#EAEEFE_66%)] overflow-x-clip">
+    <section
+      ref={heroRef}
+      className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183ec2,#EAEEFE_66%)] overflow-x-clip"
+    >
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-119.5">
-            <div className="tag">
-              Version 2.0 is here
-            </div>
+            <div className="tag">Version 2.0 is here</div>
             <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-linear-to-b from-black to-[#001e80] text-transparent bg-clip-text">
               Pathway to productivity
             </h1>
@@ -43,30 +62,45 @@ const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-162 md:flex-1 relative">
-            <Image
-              src={cogImage}
+            <motion.img
+              src={cogImage.src}
               alt="Cog image"
               className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
+              animate={{
+                translateY: [-30, 30],
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                duration: 3,
+                ease: "easeInOut",
+              }}
             />
-            <Image
-              src={cylinderImage}
+            <motion.img
+              src={cylinderImage.src}
               alt="Cylinder image"
               width={220}
               height={220}
               className="hidden md:block -top-8 -left-32 md:absolute"
+              style={{
+                translateY: translateY
+              }}
             />
-            <Image
-              src={noodleImage}
+            <motion.img
+              src={noodleImage.src}
               alt="Noodle image"
               width={220}
               className="hidden lg:block absolute top-131 left-112 rotate-30"
+              style={{
+                translateY: translateY,
+                rotate: 30
+              }}
             />
           </div>
         </div>
       </div>
     </section>
   );
-}
-
+};
 
 export default Hero;
